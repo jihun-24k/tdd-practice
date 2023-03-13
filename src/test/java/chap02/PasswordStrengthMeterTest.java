eb015d528aa2e09464df290a7cabd31d772d3bfc
jihun-24k <gi020477@gmail.com>
2023-03-13 21:47:27 +0900

    Test: 암호강도 체크 테스트 추가

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,11 +1,15 @@
 package chap02;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
 
     @Test
-    void name() {
-
+    void meetsAllCriteria_Then_Strong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        int result = meter.meter("ab12!@AB");
+        assertEquals(2, result);
     }
 }
