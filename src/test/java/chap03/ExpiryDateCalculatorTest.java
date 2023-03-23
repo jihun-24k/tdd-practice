20fcfb9fe03a6c0ef10f4b52861baf95cdd7d626
jihun-24k <gi020477@gmail.com>
2023-03-23 22:21:29 +0900

    Refactor: PayData 객체 생성해서 리팩토링

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -9,20 +9,20 @@ public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
 
-        assertExpiryDate(LocalDate.of(2023, 3,1), 10_000, LocalDate.of(2023, 4,1));
-        assertExpiryDate(LocalDate.of(2023, 5,5), 10_000, LocalDate.of(2023, 6,5));
+        assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2023,3,1)).payAmount(10_000).build(), LocalDate.of(2023, 4,1));
+        assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2023,5,5)).payAmount(10_000).build(), LocalDate.of(2023, 6,5));
     }
 
     @Test
     void 납부일과_한달_뒤_일자가_같지_않음() {
-        assertExpiryDate(LocalDate.of(2023, 1, 31), 10_000, LocalDate.of(2023, 2, 28));
-        assertExpiryDate(LocalDate.of(2023, 5, 31), 10_000, LocalDate.of(2023, 6, 30));
-        assertExpiryDate(LocalDate.of(2024, 1, 31), 10_000, LocalDate.of(2024, 2, 29));
+        assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2023,1,31)).payAmount(10_000).build(), LocalDate.of(2023, 2, 28));
+        assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2023,5,31)).payAmount(10_000).build(), LocalDate.of(2023, 6, 30));
+        assertExpiryDate(PayData.builder().billingDate(LocalDate.of(2024,1,31)).payAmount(10_000).build(), LocalDate.of(2024, 2, 29));
     }
 
-    void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+    void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
         assertEquals(expectedExpiryDate, realExpiryDate);
     }
 }
