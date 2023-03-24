e1819449ad9ddc63b901f707ad8e649e3a05757a
jihun-24k <gi020477@gmail.com>
2023-03-24 15:44:35 +0900

    Refactor: 만료일 변수화

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -3,13 +3,14 @@ package chap03;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
+    int addedMonths = 1;
     public LocalDate calculateExpiryDate(PayData payData) {
         if (payData.getFirstBillingDate() != null){
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if(payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
-        return payData.getBillingDate().plusMonths(1);
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
 }
